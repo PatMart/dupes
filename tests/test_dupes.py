8bbefaaa7a4dd05ee21b270386441f272c739be5
Devin Kelly <dwwkelly@fastmail.fm>
2015-03-12 23:04:37 -0400

    tests get_files function

--- tests/test_dupes.py
+++ tests/test_dupes.py
@@ -8,9 +8,11 @@ test_dupes
 Tests for `dupes` module.
 """
 
+
+import mock
 import unittest
 
-from dupes import dupes
+from dupes import Dupes
 
 
 class TestDupes(unittest.TestCase):
@@ -18,11 +20,19 @@ class TestDupes(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_something(self):
-        pass
-
     def tearDown(self):
         pass
+
+    def test_get_files(self):
+
+        d = Dupes()
+        with mock.patch('dupes.os.listdir') as mock_listdir:
+            with mock.patch('dupes.os.path.isfile') as mock_isfile:
+                mock_listdir.return_value = ['a', 'b', 'c']
+                mock_isfile.return_value = True
+                d.get_files()
+
+        self.assertEqual(d.files, ['a', 'b', 'c'])
 
 if __name__ == '__main__':
     unittest.main()
